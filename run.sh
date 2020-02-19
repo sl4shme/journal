b0e1a01601cc0322e8d9e9d028c6d568191b4292
Pierre Mathieu <pi3rra@root.gg>
2020-02-19 01:27:22 +0100

    Added log display to run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 trap on_sigint SIGINT
+trap on_sigusr SIGUSR1
 
-killed=0
+killed_int=0
+killed_usr=0
 
 function on_sigint() {
-    killed=1    
+    killed_int=1    
+}
+
+function on_sigusr() {
+    killed_usr=1    
 }
 
 function watch_file() {
@@ -14,31 +20,38 @@ function watch_file() {
         last_mdate=`stat -c %y $1`
         if [[ "$mdate" != "$last_mdate" ]] ; then
             mdate=$last_mdate
-            kill -s SIGINT $2
+            kill -s $3 $2
         fi
     done
 }
 
-watch_file ./app/templates/form.html $$ &
-watch_file ./app/routes.py $$ &
-watch_file ./journal.py $$ &
+watch_file ./app/templates/form.html $$ SIGINT &
+watch_file ./app/routes.py $$ SIGINT &
+watch_file ./journal.py $$ SIGINT &
+
+watch_file ./flask.log $$ USR1 &
 
 flask run >> ./flask.log 2>&1 &
 flask_pid=$!
 
 while true ; do
-    if [[ "$killed" == "1" ]] ; then
+    if [[ "$killed_int" == "1" ]] ; then
         kill $flask_pid
         flask run >> ./flask.log 2>&1 &
         flask_pid=$!
-        echo "Reloaded. Press 'c' again to kill"
+        echo -e "\n\nReloaded. Press 'c' again to kill"
         read -rsn 1 -t 2 answer
         if [[ "$answer" == "c" ]] ; then
             kill $flask_pid
             exit 0
         fi
-        killed=0
-    else
-        sleep 0.1
+        killed_int=0
+        killed_usr=1
     fi
+    if [[ "$killed_usr" == "1" ]] ; then
+        clear
+        tail -n 5 flask.log
+        killed_usr=0
+    fi
+    sleep 0.1
 done
